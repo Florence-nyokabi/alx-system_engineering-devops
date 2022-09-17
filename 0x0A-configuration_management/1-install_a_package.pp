0c5f463d0e63f8572762fe2c3c1d56f68d23b9ed
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2022-09-17 16:34:42 +0300

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-# installs the package puppet-lint
+# Install puppet-lint
 package { 'puppet-lint':
-  ensure   => '2.1.0',
-  provider => 'pip',
-  name     => 'puppet-lint'
+  ensure   => '2.1.1',
+  name     => 'puppet-lint',
+  provider => 'gem'
 }
